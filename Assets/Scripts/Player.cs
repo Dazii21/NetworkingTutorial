b69b92df55cb7fe10a2eda3e545b22f5c80a5d38
Dazii21 <DaziVal@outlook.com>
2022-06-25 23:11:31 -0700

    I want to commit not live anymore Lol

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,5 +19,35 @@ public class Player : NetworkBehaviour
     void Update()
     {
         HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Sending Hola to Server!");
+            Hola();
+        }
+
+        if (isServer && transform.position.y > 50)
+        {
+            TooHigh();
+        }
+    }
+    
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Received Hola from Client!");
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Recieved Hola from server!");
+    }
+
+    [ClientRpc]
+    void TooHigh()
+    {
+        Debug.Log("Too High!");
     }
 }
